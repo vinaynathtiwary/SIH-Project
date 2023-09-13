ba04a32319275b363a0783d9dd36a788537e9ade
vinaynathtiwary <vinaynathtiwary@gmail.com>
2023-09-13 17:29:48 +0530

    preConfranceApp

---
+++ mainFile/script.js
@@ -0,0 +1,14 @@
+function redirectToFormPage() {
+  var targetPageUrl = "form.html";
+  window.location.href = targetPageUrl;
+}
+
+function redirectToAdminPage() {
+  var targetPageUrl = "adminLogin.html";
+  window.location.href = targetPageUrl;
+}
+
+function redirectToUserPage() {
+  var targetPageUrl = "userLogin.html";
+  window.location.href = targetPageUrl;
+}
